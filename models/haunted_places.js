23977f6c971c6b446b9d2379a607592dbb4473d5
GuanMingChee <guanmingchee11@gmail.com>
2021-12-01 18:23:42 -0600

    part5 done

--- models/haunted_places.js
+++ models/haunted_places.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 const haunted_placeSchema = mongoose.Schema({
-address: String,
-year: Number,
+address: {
+    type: String,
+},
+year: {
+    type: Number,
+    min: [10, "bigger number"]
+},
 description: String
 })
 module.exports = mongoose.model("Haunted_places", haunted_placeSchema)
