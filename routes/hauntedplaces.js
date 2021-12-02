61e82f318b5b11715463a63247e6840f0ffb73db
GuanMingChee <guanmingchee11@gmail.com>
2021-12-01 18:05:29 -0600

    part4 done

--- routes/hauntedplaces.js
+++ routes/hauntedplaces.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
 /* GET home page. */
 /**
 router.get('/', function(req, res, next) {
@@ -20,7 +30,7 @@ router.get('/detail', haunted_places_controllers.haunted_places_view_one_Page);
 /* GET create costume page */
 router.get('/create', haunted_places_controllers.haunted_places_create_Page);
 /* GET create update page */
-router.get('/update', haunted_places_controllers.haunted_places_update_Page);
+router.get('/update', secured, haunted_places_controllers.haunted_places_update_Page);
 /* GET create costume page */
 router.get('/delete', haunted_places_controllers.haunted_places_delete_Page);
 module.exports = router;
